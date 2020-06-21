396cb2686b2f8b1c03642f84848ee5991dd85bbf
tat-shkoda <tat.shkoda@gmail.com>
2020-06-21 11:24:19 +0400

    Update Response.py

--- Response.py
+++ Response.py
@@ -1,20 +1,12 @@
 import time
 
 import requests
-from lxml import html
-
 from urllib3 import disable_warnings, exceptions
-
-from Proxy import get_active_proxy
-
 
 disable_warnings(exceptions.InsecureRequestWarning)
 
 def search_captcha(response):
-    tree = html.document_fromstring(response.content)
-    if tree.xpath("//div[@class='firewall-container']"):
-        return True
-    return False
+    pass
 
 
 def send_request(page_url):
@@ -27,7 +19,6 @@ def send_request(page_url):
         }
 
         try:
-            get_active_proxy()
             response = requests.get(
                 page_url,
                 headers=headers,
@@ -57,44 +48,4 @@ def send_request(page_url):
             if attempts == 3:
                 return
 
-            continue
-
-
-def send_request_with_proxy(page_url):
-    is_successfull = False
-    while is_successfull == False:
-        print(page_url)
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36',
-        }
-
-        try:
-            proxy = get_active_proxy()
-
-            proxies = {
-                'http': 'http://' + proxy,
-                'https': 'https://' + proxy,
-            }
-            response = requests.get(
-                page_url,
-                headers=headers,
-                proxies=proxies,
-                timeout=15
-            )
-
-            if response.status_code == 404:
-                return 404
-
-            if response.status_code == 200:
-                if search_captcha(response):
-                    print('Найдена капча', page_url)
-                    time.sleep(600)
-                    continue
-                else:
-                    is_successfull = True
-                    response.encoding = 'cp-1251'
-                    return response
-
-        except Exception as e:
-            print('Ошибка соединения', page_url, e)
             continue
